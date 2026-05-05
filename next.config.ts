422f42d4f3f3e336c2490d777e1efdd4a8f07f18
panjiteknologi <itserv.panji@gmail.com>
2026-05-05 17:52:54 +0700

    fixing generate ppt

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["playwright", "playwright-core", "@sparticuz/chromium"],
+  outputFileTracingIncludes: {
+    "/api/generate-pptx": ["./node_modules/@sparticuz/chromium/**/*"],
+  },
 };
 
 export default nextConfig;
